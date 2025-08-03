410c4288d54ca5c6f5613ec80882c5ce930fa26b
DhavalTank <tankdhaval3041@gmail.com>
2025-08-03 17:39:48 +0530

    DT:Moved api endpoint in env

--- graphql/client.ts
+++ graphql/client.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+// const GRAPHQL_API_ENDPOINT = process.env.GRAPHQL_API_ENDPOINT;
+
+// console.log({ GRAPHQL_API_ENDPOINT });
 export const client = new ApolloClient({
-  uri: process.env.NEXT_GRAPHQL_API_ENDPOINT,
+  uri: "https://saleor.kombee.co.in/graphql/",
   cache: new InMemoryCache(),
 });
